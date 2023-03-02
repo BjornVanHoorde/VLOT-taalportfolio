435af31268e3f334699b9ca64f25e188f10765e3
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2023-03-02 16:04:32 +0100

    Frontend vaardigheden aangevult
    Frontend elementen toegevoegd.
    styling toegevoegd

--- app/src/components/App/Screens/Taalgroei/Vaardigheden/Vaardigheden.js
+++ app/src/components/App/Screens/Taalgroei/Vaardigheden/Vaardigheden.js
@@ -1,14 +1,21 @@
+import verwijder from "../../../../../img/delete.svg";
+
+const newButton = () => {
+
+}
+
 const Vaardigheden = () => {
     return (
         <div className="taalgroei-sequence">
             <div className="taalgroei-nav">
                 <nav>
-                    <button className="taalgroei-nav-button">Spreken</button>
-                    <button className="taalgroei-nav-button">Schrijven</button>
-                    <button className="taalgroei-nav-button">Lezen</button>
-                    <button className="taalgroei-nav-button">Luisteren</button>
+                    <button className="taalgroei-nav-button"><p>Spreken</p></button>
+                    <button className="taalgroei-nav-button"><p>Schrijven</p></button>
+                    <button className="taalgroei-nav-button"><p>Lezen</p></button>
+                    <button className="taalgroei-nav-button"><p>Luisteren</p></button>
                 </nav>
             </div>
+            <div className="lijn"></div>
             <div className="vaardigheden-content">
                 <div className="evaluatie-sidebar">
                     <div className="evaluatie-add">
@@ -19,6 +26,17 @@ const Vaardigheden = () => {
                             <strong>+</strong>
                         </div>
                     </div>
+                    <div className="evaluatie-item">
+                    <div className="evaluatie-add-tekst">
+                            <span>Evaluatie 1</span>
+                        </div>
+                        <div className="evaluatie-delete">
+                            <img src={verwijder} alt='delete'></img>
+                        </div>
+                    </div>
+                </div>
+                <div className="content">
+                    <h3 className="no-content">Er werden nog geen evaluaties aangemaakt</h3>
                 </div>
             </div>
         </div>
